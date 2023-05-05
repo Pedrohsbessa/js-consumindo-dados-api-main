ba7da363c54e82b8f3fc1f7535bcd34760ed5d1b
pedrohsbessa <pedrohsbessa@gmail.com>
2023-05-05 01:01:28 -0300

    Criando o auto preenchimento de dados captados do cep

--- scripts.js
+++ scripts.js
@@ -1,17 +1,35 @@
 async function endereço(cep) {
+    const erroMsg = document.querySelector('#erro');
+    erroMsg.innerHTML = '';
     try {
-
         const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         const result = await consultaCep.json()
         if (result.erro) {
             throw Error('cep inexistente')
         }
-        return result
+        const cidade = document.querySelector('#cidade')
+        const bairro = document.querySelector('#bairro')
+        const logradouro = document.querySelector('#endereco')
+        const estado = document.querySelector('#estado')
+
+        cidade.value = result.localidade;
+        logradouro.value = result.logradouro;
+        estado.value = result.uf;
+        bairro.value = result.bairro;
 
         console.log(result);
+        return result
+
     } catch (erro) {
+        erroMsg.innerHTML = `<p>Cep Inválido tente novamente</p>`
+        cidade.value = ''
+        bairro.value = ''
         console.log(erro);
     }
 }
 
-endereço()
+const cep = document.querySelector('#cep')
+cep.addEventListener("focusout", () => {
+    endereço(cep.value)
+
+}) 
